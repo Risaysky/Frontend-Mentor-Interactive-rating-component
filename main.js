1b493634623fa807d1c2503ceb4dd7c8e6865adf
Risaysky <yasserasklou92@gmail.com>
2025-06-08 16:10:03 +0100

    Add invalid state

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 const submitBtn = document.querySelector(".submit-btn");
 const ratingCard = document.querySelector(".rate-us");
 const thanksCard = document.querySelector(".thank-you");
-const radioBtns = document.querySelectorAll('input[name="rating"]');
+const ratingSelection = document.querySelector(".rating-selection");
+const radioBtns = ratingSelection.children;
 const ratingAnnouncement = document.querySelector(".rating-announcement");
 
 submitBtn.addEventListener("click", validateRating);
 
 function validateRating() {
     for (let i = 0; i < 5; i++) {
-        if (!radioBtns[i].checked) {
+        if (!radioBtns[i].children[0].checked) {
             continue;
         } else {
             showNextCard(i);
             return;
         }
     }
-    console.log("please select");
+    ratingSelection.toggleAttribute("data-invalid")
+    setTimeout(() => ratingSelection.toggleAttribute("data-invalid"), 500);
+    setTimeout(() => ratingSelection.toggleAttribute("data-invalid"), 900);
+    setTimeout(() => ratingSelection.toggleAttribute("data-invalid"), 1400);
 }
 
 function showNextCard(rating) {
